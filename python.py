55d468432265be5cabfdd97f1ba7fd53c072e680
0azis <123@gmail.com>
2023-01-15 20:18:55 +0300

    new file

---
+++ python.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
+
+x = np.random.randint(100, size=10)
+y = np.random.randint(100,size=10)
+
+lreg = stats.linregress(x, y)
+print(lreg)
+
+plt.scatter(x, y)
+plt.plot(x, lreg.intercept + lreg.slope*x)
+plt.show()
